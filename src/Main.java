534385edc3133da7696fa84ba75defa038e85846
MatiasErra <matiaserramouspe@gmail.com>
2022-10-15 22:24:54 -0300

    primera mitad v2

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ public class Main {
      static List<String> listaJ = new ArrayList<>();
      static List<String> listaE = new ArrayList<>();
      static List<String> listaD = new ArrayList<>();
+    static List<String> listaA = new ArrayList<>();
+    static List<String> listaJE = new ArrayList<>();
     static Scanner scan = new Scanner(System.in);
     static List<Jugador> listaJugador = new ArrayList<>();
     static List<DT> listaDT = new ArrayList<>();
@@ -27,6 +29,8 @@ public class Main {
          leerArchivoJ();
          leerArchivoE();
          leerArchivoDT();
+         leerArchivoJE();
+        leerArchivoA();
         while (opcionPrincipal != 0) {
             System.out.println("Seleccione una opcion del menu:\n1. Administrar personas.\n2. Administrar equipos.\n3. Iniciar un partido.\n4. Listar partidos.\n0. Cerrar.");
             opcionPrincipal = scan.nextInt();
@@ -132,8 +136,9 @@ public class Main {
                  String  id =arregloF[0];
                  String  nombre =arregloF[1].toUpperCase();
                  String  Dt =arregloF[2].toUpperCase();
-
-                 Equipo Pequipo = new Equipo(id, nombre, Dt);
+                 int T = Integer.parseInt(arregloF[3]);
+                    int S = Integer.parseInt(arregloF[4]);
+                 Equipo Pequipo = new Equipo(id, nombre, Dt, T, S);
                  listaEquipo.add(Pequipo);
 
 
@@ -155,8 +160,107 @@ public class Main {
         }
     }
 
+    public static void leerArchivoA() {
+        FileReader fr = null;
+        try {
+            fr = new FileReader("Arbitros.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
 
-     public static void leerArchivoDT() {
+            while ((linea = br.readLine()) != null) {
+
+                String l =  linea.toLowerCase();
+                listaA.add(l);
+            }
+
+            String[] arreglo = new String[listaA.size()]; // Copiar el contenido de la lista a un arreglo de Strings
+
+
+
+            String[] arregloF =  new String[listaA.size()];
+            for (int i = 0; i < listaA.size(); i++) {
+
+                arreglo[i] = listaA.get(i);
+
+                arregloF = arreglo[i].split(" - ");
+                String  id =arregloF[0];
+                String  nombre =arregloF[1].toUpperCase();
+                String  apellido =arregloF[2].toUpperCase();
+                String  puesto =arregloF[3].toUpperCase();
+                Arbitro Parbitro = new Arbitro(id, nombre, apellido, puesto);
+                listaArbitro.add(Parbitro);
+
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {// Nos asegurarnos que se cierra el archivo.
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void leerArchivoJE() {
+        FileReader fr = null;
+        try {
+            fr = new FileReader("JugadoresEquipo.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                String l =  linea.toLowerCase().trim();
+                listaJE.add(l);
+            }
+
+            String[] arreglo = new String[listaJE.size()]; // Copiar el contenido de la lista a un arreglo de Strings
+
+
+
+            String[] arregloF =  new String[listaJE.size()];
+            for (int i = 0; i < listaJE.size(); i++) {
+
+                arreglo[i] = listaJE.get(i);
+
+                arregloF = arreglo[i].split(" - ");
+                String  idJug =arregloF[0];
+                String  idEqu =arregloF[1].toUpperCase();
+                int num = Integer.parseInt(arregloF[2].toUpperCase());
+
+                JugadorEquipo PJe = new JugadorEquipo(idJug, idEqu, num);
+                listaJugadorEquipo.add(PJe);
+
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {// Nos asegurarnos que se cierra el archivo.
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static void leerArchivoDT() {
          FileReader fr = null;
          try {
              fr = new FileReader("DT.txt");
@@ -286,21 +390,214 @@ public class Main {
     }
 
     public static void crearPartido() {
+        List<Equipo> listaEquipoListo = new ArrayList<>();
+        String[] rndm = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        int cont = 0;
+        int j = PartidoCont();
+        String id = "";
+        for (Equipo unE : listaEquipo) {
+            if (unE.getCantJugT() == 11 && unE.getCantJugS() == 5) {
+                listaEquipoListo.add(unE);
+            }
+        }
+        System.out.println("Los equipos disponibles para un partido son : ");
+        for (Equipo unEl : listaEquipoListo) {
+            System.out.println(cont + " " + unEl.toString());
+            cont++;
+        }
+        if (cont == 0) {
+            System.out.println("No hay un equipo con la cantidad de jugadores para comenzar un partido");
+        } else {
+            System.out.println("Selecionar Equipo Visitante");
+            int eq1 = scan.nextInt();
+            Equipo Vistiante = listaEquipoListo.get(eq1);
+            try {
+                listaEquipoListo.remove(eq1);
+
+            } catch (Exception e) {
+                System.out.println("No existe el equipo selecionado");
+            }
+            cont =0;
+            for (Equipo unEl : listaEquipoListo) {
+                System.out.println(cont + " " + unEl.toString());
+                cont++;
+            }
+
+            System.out.println("Selecionar Equipo Local");
+            int eq2 = scan.nextInt();
+            Equipo Local = listaEquipoListo.get(eq2);
+            try {
+                listaEquipoListo.remove(eq2);
+
+            } catch (Exception e) {
+                System.out.println("No existe el equipo selecionado");
+            }
+            int contP = 0;
+            while (contP == 0) {
+                id = "";
+                for (int i = 0; i < 16; i++) {
+                    id += rndm[(int) (Math.random() * 10)];
+                }
+                if(j != 0) {
+                    for (Partido unPar : listaPartido) {
+
+
+
+                        if (unPar.getId().equals(id)) {
+                            break;
+                        } else {
+                            contP++;
+                        }
+                    }
+                }
+                else
+                {
+                    contP++;
+                }
+            }
+            System.out.println("Ingrese el estadio del partido");
+            String Est = scan.next();
+            System.out.println("Ingrese el dia del partido");
+            short dia = scan.nextShort();
+            System.out.println("Ingrese la hora del partido");
+            String Hora = scan.next();
+            System.out.println("Ingrese el clima del partido");
+            String Clima = scan.next();
+
+            Partido newP = new Partido(id, Est, dia,Hora,Clima,Local.getId(),Vistiante.getId());
+            int opcion = 1;
+            MostrarPartido(id,Local.getId(),Vistiante.getId());
+
+            while (opcion != 0) {
+                System.out.println("Seleccione una accion del partido:\n1. Anotar gol.\n2. Cambio de jugador.\n0. Finalizar partido.");
+                opcion = scan.nextInt();
+                switch (opcion) {
+                    case 0:
+
+                        break;
+                    case 1:
+                        altaGol(id,Local.getId(),Vistiante.getId());
+                        break;
+                    case 2:
+                        cambioJugador(id);
+                        break;
+
+                    default:
+                        System.out.println("¡Seleccione una opcion valida!");
+                        break;
+                }
+            }
+
+
+
+
+
+        }
+    }
+
+    public static void altaGol(String id, String local, String visitantes) {
+        System.out.println("El gol fue del equipo local o visitante  ");
+        String equipo = String.valueOf(scan.next().toLowerCase().charAt(0));
+        List<Jugador> listaJugadorESelct = new ArrayList<>();
+        if (equipo.equals("l")) {
+
+            int contG = 0;
+
+            for (JugadorEquipo unJE : listaJugadorEquipo) {
+                for (Jugador unJugador : listaJugador) {
+                    if (unJE.getIdEquipo().equals(local) && unJugador.getTipo().equals("T") &&
+                            unJE.getIdJugador().equals(unJugador.getId())) {
+
+                        System.out.println(contG + " " + unJugador.getNombre() + " " + unJugador.getApellido());
+                        contG++;
+                        listaJugadorESelct.add(unJugador);
+                    }
+                }
+            }
+
+        }
+        if (equipo.equals("v")) {
+            {
+
+                int contG = 0;
+
+                for (JugadorEquipo unJE : listaJugadorEquipo) {
+                    for (Jugador unJugador : listaJugador) {
+                        if (unJE.getIdEquipo().equals(visitantes) && unJugador.getTipo().equals("T") &&
+                                unJE.getIdJugador().equals(unJugador.getId())) {
+
+                            System.out.println(contG + " " + unJugador.getNombre() + " " + unJugador.getApellido());
+                            contG++;
+                            listaJugadorESelct.add(unJugador);
+                        }
+                    }
+                }
+
+            }
+
+        }
+        System.out.println("Ingrese el jugador que ingreso el gol");
+        int jug =  scan.nextInt();
+
+try {
+            Gol newG = new Gol(listaJugadorESelct.get(jug).getId(), id);
+            System.out.println("El gol se registro con exito");
+        }
+        catch(Exception e)
+        {
+            System.out.println("No se pudo registrar el gol");
+        }
+    }
+    public static void cambioJugador(String id) {
 
     }
 
-    public static void listarPartidos() {
+    public static void MostrarPartido(String id, String local, String visitantes)
+    {
+        String J = "";
 
+        for(JugadorEquipo newJQ :listaJugadorEquipo){
+        for(Jugador unJugador : listaJugador) {
+
+                if (newJQ.getIdEquipo().equals(local) && newJQ.getIdJugador().equals(unJugador.getId())) {
+                    J = "Local";
+                    System.out.println(J + " " + unJugador.toString());
+                }
+                if(newJQ.getIdEquipo().equals(visitantes) && newJQ.getIdJugador().equals(unJugador.getId()))
+            {
+                J = "Visitantes";
+                System.out.println(J + " " + unJugador.toString());
+            }
+            }
+        }
+
+    }
+
+
+    public static void listarPartidos() {
+        int cont = 0;
+ for(Partido Ppar : listaPartido){
+     cont++;
+ }
+ if(cont==0)
+ {
+     System.out.println("No existen partidos registrados");
+ }
+    }
+
+    public static int PartidoCont() {
+        int cont = 0;
+        for(Partido Ppar : listaPartido){
+            cont++;
+        }
+  return  cont;
     }
 
     //endregion
 
     //region personas
 
-    public static void buscarJugador()
-    {
 
-    }
     public static void altaJugador() {
         String[] rndm = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String id = "";
@@ -808,17 +1105,19 @@ public class Main {
 
  int ClJQ = 0;
                for(JugadorEquipo unJQ : listaJugadorEquipo) {
-               if(unJQ.getIdEquipo() == E.getId() && unJQ.getNumero() == numJ)
+               if(unJQ.getIdEquipo().equals(E.getId())  && unJQ.getNumero() == numJ)
                {
                    ClJQ++;
               }
           }
-                   if (E.getCantJugT() < 11 && J.getTipo().equals("T")) {
-
+               int i  =E.getCantJugT();
+           if (E.getCantJugT() > 11 && J.getTipo().equals("T")) {
+                       System.out.println("Ya se ingreso la cantidad maxima de Titulares");
+                   }
                        if(ClJQ>0) {
                        System.out.println("Ya existe este numero en el equipo");
-                      }
-                       else {
+                       }
+                        if (E.getCantJugT() <= 11 && J.getTipo().equals("T")) {
                            try {
                                 JugadorEquipo pJugadorEquipo = new JugadorEquipo(idJugador, idEquipo, numJ);
                                listaJugadorEquipo.add(pJugadorEquipo);
@@ -828,9 +1127,11 @@ public class Main {
                           }
                       }
 
-              }
-                   if (E.getCantJugS() < 5 && J.getTipo().equals("S")) {
 
+                   if (E.getCantJugS() > 5 && J.getTipo().equals("S")) {
+                       System.out.println("Ya se ingreso la cantidad maxima de Suplentes");
+                   }
+                   if ((E.getCantJugS() <= 5 && J.getTipo().equals("S"))) {
                       try {
                            JugadorEquipo pJugadorEquipo = new JugadorEquipo(idJugador, idEquipo, numJ);
                           listaJugadorEquipo.add(pJugadorEquipo);
@@ -854,15 +1155,7 @@ public class Main {
 
 
 
-     public static String NomDelJ(String idJ) {
-          String nom = "";
-         for (Jugador unJugador : listaJugador) {
-             if (unJugador.getId() == idJ) {
-                 nom = unJugador.getNombre();
-             }
-         }
-         return  nom;
-     }
+
 
 
     public static void bajaJugadorEquipo(){
@@ -901,7 +1194,15 @@ try  {
                      }
                     try {
                          listaJugadorEquipo.remove(EQ);
-                        System.out.println("Jugador en equipo eliminado.");
+                        System.out.println("Jugador de equipo eliminado.");
+                        for(Jugador Pjug : listaJugador) {
+                            if (EQ.getIdJugador().equals(Pjug.getId()) && Pjug.getTipo().equals("T"))
+                            {
+                                listaEquipo.get(numE).setCantJugT(listaEquipo.get(numE).getCantJugT() -1);
+                                System.out.println(listaEquipo.get(numE).getCantJugT());
+                            }
+                        }
+
                     } catch (Exception e) {
                         System.out.println("El jugador en equipo no existe.");
                     }
@@ -932,6 +1233,7 @@ try  {
     public  static  void  ListarJugadorEquipo(){
          int contE =  ContEquipos();
          listarEquipos();
+         String Nom  = "";
  String nom = "";
      int cont = 0;
 
@@ -945,7 +1247,8 @@ try  {
                  for (JugadorEquipo unJE : listaJugadorEquipo) {
 
                      if (listaEquipo.get(num).getId().equals(unJE.getIdEquipo())) {
-                         System.out.println(cont + ". " +  NomDelJ(unJE.getIdJugador()) + "  ");
+                         Nom = NomDelJ(unJE.getIdJugador());
+                         System.out.println(cont + ". " +  Nom+ "  ");
                          cont++;
                      }
 
@@ -961,6 +1264,20 @@ try  {
              System.out.println("No existen jugadores para este equipo");
          }
      }
+    public static String NomDelJ(String idJ) {
+        String nom = "";
+        String ape = "";
+        String Resultado = "";
+        for (Jugador unJugador : listaJugador) {
+            if (unJugador.getId().equals(idJ)) {
+                nom = unJugador.getNombre();
+                ape = unJugador.getApellido();
+                Resultado = nom + " " + ape;
+                break;
+            }
+        }
+        return  Resultado;
+    }
 
 
 
